fa5759892b59695e6741a61379856be459c93221
Mehdi Cherti <mehdicherti@gmail.com>
2018-12-22 11:16:50 +0100

    first commit

---
+++ cli.py
@@ -0,0 +1,133 @@
+import os
+from clize import run
+from functools import partial
+import shutil
+from skimage.io import imsave
+
+import torch
+from torch.autograd import Variable
+import torch.optim as optim
+import torchvision.utils as vutils
+
+from viz import grid_of_images_default
+
+from model import VAE
+from model import loss_function
+
+from data import load_dataset, PatchDataset
+
+
+def train(*,
+          folder='out',
+          dataset='mnist',
+          patch_size=None,
+          resume=False,
+          log_interval=1,
+          device='cpu',
+          batch_size=64,
+          nz=100,
+          parent_model=None,
+          freeze_parent=False,
+          num_workers=1,
+          nb_filters=64,
+          nb_draw_layers=1):
+    try:
+        os.makedirs(folder)
+    except Exception:
+        pass
+    lr = 0.0002
+    nb_epochs = 3000
+    dataset = load_dataset(dataset, split='train')
+    if patch_size is not None:
+        patch_size = int(patch_size)
+        dataset = PatchDataset(dataset, patch_size)
+    x0, _ = dataset[0]
+    nc = x0.size(0)
+    w = x0.size(1)
+    h = x0.size(2)
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+    )
+    act = 'sigmoid' if nc == 1 else 'tanh'
+    if resume:
+        net = torch.load('{}/net.th'.format(folder))
+    else:
+        if parent_model:
+            parent = torch.load(parent_model)
+        else:
+            parent = None
+        net = VAE(latent_size=nz, nc=nc, w=patch_size,
+                  act=act, ndf=nb_filters, parent=parent)
+    opt = optim.Adam(net.parameters(), lr=lr, betas=(0.5, 0.999))
+    input = torch.FloatTensor(batch_size, nc, w, h)
+    input = input.to(device)
+    net = net.to(device)
+    niter = 0
+    for epoch in range(nb_epochs):
+        for i, (X, _), in enumerate(dataloader):
+            net.zero_grad()
+            batch_size = X.size(0)
+            X = X.to(device)
+            input.resize_as_(X).copy_(X)
+            Xrec, mu, logvar = net(input)
+            loss = loss_function(X, Xrec, mu, logvar)
+            loss.backward()
+            opt.step()
+            if niter % log_interval == 0:
+                print(f'Epoch: {epoch:05d}/{nb_epochs:05d} iter: {niter:05d} loss: {loss.item()}')
+            if niter % 100 == 0:
+                x = 0.5 * (X + 1) if act == 'tanh' else X
+                f = 0.5 * (Xrec + 1) if act == 'tanh' else Xrec
+                vutils.save_image(
+                    x, '{}/real_samples.png'.format(folder), normalize=True)
+                vutils.save_image(
+                    f, '{}/fake_samples_epoch_{:03d}.png'.format(folder, epoch), normalize=True)
+                torch.save(net, '{}/net.th'.format(folder))
+            niter += 1
+
+
+def train_hierarchical(*,
+                       batch_size=64,
+                       dataset='mnist',
+                       scale=8, nz=100,
+                       use_parent=True,
+                       freeze_parent=True,
+                       nb_filters=64,
+                       nb_draw_layers=1,
+                       device='cpu',
+                       log_interval=1,
+                       resume=False):
+
+    folder = os.path.join('results', dataset, '{}x{}'.format(scale, scale))
+    if os.path.exists(folder) and not resume:
+        shutil.rmtree(folder)
+    prev_scale = scale // 2
+    parent_model = os.path.join(
+        'results', dataset, '{}x{}'.format(prev_scale, prev_scale), 'gen.th')
+    if not os.path.exists(parent_model):
+        use_parent = False
+    params = dict(
+        dataset=dataset,
+        folder=folder,
+        patch_size=scale,
+        nz=nz,
+        nb_filters=nb_filters,
+        batch_size=batch_size,
+        nb_draw_layers=nb_draw_layers,
+        device=device,
+        resume=resume,
+        log_interval=log_interval
+    )
+    if use_parent:
+        params.update(dict(
+            parent_model=parent_model,
+            freeze_parent=freeze_parent
+        ))
+    train(**params)
+
+
+if __name__ == '__main__':
+    run([train, train_hierarchical])
---
+++ model.py
@@ -0,0 +1,129 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.nn.init import xavier_uniform
+
+
+class VAE(nn.Module):
+
+    def __init__(self, nc=1, ndf=64, act='sigmoid', latent_size=None, w=64,nb_draw_layers=1, parent=None):
+        super().__init__()
+        if parent is None:
+            assert latent_size
+        else:
+            latent_size = parent.latent_size * 4 * nb_draw_layers
+        self.latent_size = latent_size
+        self.act = act
+        self.ndf = ndf
+        self.round = round
+        self.parent = parent
+
+        nb_blocks = int(np.log(w)/np.log(2)) - 3
+        nf = ndf
+        layers = [
+            nn.Conv2d(nc, nf, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, inplace=True),
+        ]
+        for _ in range(nb_blocks):
+            layers.extend([
+                nn.Conv2d(nf, nf * 2, 4, 2, 1, bias=False),
+                #nn.BatchNorm2d(nf * 2),
+                nn.LeakyReLU(0.2, inplace=True),
+            ])
+            nf = nf * 2
+
+        self.encoder = nn.Sequential(*layers)
+
+        wl = w // 2**(nb_blocks+1)
+        self.latent = nn.Sequential(
+            nn.Linear(nf * wl * wl, latent_size * 2),
+        )
+        self.post_latent = nn.Sequential(
+            nn.Linear(latent_size, nf * wl * wl)
+        )
+        self.post_latent_shape = (nf, wl, wl)
+        layers = []
+        for _ in range(nb_blocks):
+            layers.extend([
+                nn.ConvTranspose2d(nf, nf // 2, 4, 2, 1, bias=False),
+                #nn.BatchNorm2d(nf // 2),
+                nn.ReLU(True),
+            ])
+            nf = nf // 2
+        layers.append(
+            nn.ConvTranspose2d(nf,  nc, 4, 2, 1, bias=False)
+        )
+        self.decoder = nn.Sequential(*layers)
+        self.nb_draw_layers = nb_draw_layers
+        
+        self.encoder.apply(weights_init)
+        self.decoder.apply(weights_init)
+
+    def forward(self, input):
+        x = self.encoder(input)
+        pre_latent_size = x.size()
+        x = x.view(x.size(0), -1)
+        h = self.latent(x)
+        mu, logvar = h[:, 0:self.latent_size], h[:, self.latent_size:]
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        h = mu + eps * std
+        if self.parent:
+            parent = self.parent
+            nz = parent.latent_size
+            h = h.view(x.size(0), -1)
+            # (bs, nz * 4 * self.nb_draw_layers)
+            batch_size, nh = h.size()
+            h = h.view(batch_size * self.nb_draw_layers * 4, nz)
+            # (bs * nb_draw_layers * 4, nz)
+            h = self.parent.post_latent(h)
+            h = h.view((h.size(0),) + self.parent.post_latent_shape) 
+            o = parent.decoder(h)
+            # (bs * nb_draw_layers * 4, 3, h, w)
+            _, c, h, w = o.size()
+            o = o.view(batch_size, self.nb_draw_layers, 4, c, h,  w)
+            # (bs, nb_draw_layers, 4, c, h, w)
+            o = o.mean(dim=1)
+            # (bs, 4, c, h, w)
+            h1 = o[:, 0]
+            h2 = o[:, 1]
+            h3 = o[:, 2]
+            h4 = o[:, 3]
+            h1 = h1.contiguous()
+            h2 = h2.contiguous()
+            h3 = h3.contiguous()
+            h4 = h4.contiguous()
+            oa = torch.cat((h1, h2), 3)
+            ob = torch.cat((h3, h4), 3)
+            o = torch.cat((oa, ob), 2)
+            return o, mu, logvar
+        else:
+            x = self.post_latent(h)
+            x = x.view(pre_latent_size)
+            xrec = self.decoder(x)
+            return xrec, mu, logvar
+
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+    elif classname == 'Linear':
+        nn.init.xavier_uniform_(m.weight.data)
+        m.bias.data.fill_(0)
+
+def loss_function(x, xrec, mu, logvar):
+    mse = ((xrec - x) ** 2).sum()
+    kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return mse + kld
+
+
+if __name__ == '__main__':
+    parent = VAE(nc=3, w=8, latent_size=10)
+    model = VAE(nc=3, w=16, parent=parent)
+    x = torch.rand(1, 3, 16, 16)
+    xrec, mu, logvar = model(x)
+    print(xrec.size())
