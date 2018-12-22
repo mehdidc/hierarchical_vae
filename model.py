ed1c6e4877566fe90e2746d01e6887ea5581dc65
Mehdi Cherti <mehdicherti@gmail.com>
2018-12-22 12:46:15 +0100

    update

--- model.py
+++ model.py
@@ -61,7 +61,7 @@ class VAE(nn.Module):
         return chain(self.encoder.parameters(), self.decoder.parameters())
     
     def sample(self, nb_examples=1):
-        device = self.get_device() if hasattr(self, 'get_device') else 'cpu'
+        device = next(model.parameters()).device
         z = torch.randn(nb_examples, self.latent_size).to(device)
         return self.decode(z)
 
